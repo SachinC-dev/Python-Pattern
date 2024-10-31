df8e8bfd73001d52f0359559ce728b566c5f56c4
Sachin <isachinsc2008@gmail.com>
2024-10-31 10:32:24 +0530

    New logic

--- pattern.py
+++ pattern.py
@@ -1,8 +1,8 @@
 n = int(input("Enter number: "))
 for i in range(1, n+1):
     print(" "*(n-i), end="")
-    for j in range(1, i+1):
-        print(j, end='')
-    for e in range(i-1, 0, -1):
+    for k in range(i):
+        print(i-k, end="")
+    for e in range(2, i+1):
         print(e, end="")
     print()
